651f679450f42db889c6f855ccc8925b0285922d
Le-Vu Tran <vu.tran@employmenthero.com>
2025-03-08 22:08:40 +0700

    Port my configs

---
+++ lua/plugins/-extras-lang-typescript.lua
@@ -0,0 +1,41 @@
+vim.g.lazyvim_eslint_auto_format = false
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = {
+          settings = {
+            vtsls = {
+              enableMoveToFileCodeAction = false,
+              completion = {
+                enableServerSideFuzzyMatch = true,
+                entriesLimit = 3000,
+              },
+            },
+            typescript = {
+              format = {
+                enable = false,
+              },
+              tsserver = {
+                maxTsServerMemory = 32768,
+              },
+              preferences = {
+                importModuleSpecifier = "project-relative",
+              },
+            },
+            javascript = {
+              format = {
+                enable = false,
+              },
+              preferences = {
+                importModuleSpecifier = "project-relative",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
